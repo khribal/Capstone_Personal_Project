2a50792574efbe36a9d43886e4c6e0f3bdcbd9fd
klhribal <klhribal@iu.edu>
2023-09-24 21:50:11 -0400

    Adding button and JS functionality

--- home.php
+++ home.php
@@ -3,15 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Style Sheets -->
     <link rel="stylesheet" type="text/css" href="/css/styles.css">
+
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Document</title>
+    
+    <title>Home Page</title>
 </head>
+
 <body>
+
+<!-- Navbar -->
 <?php
 include('includes/navbar.php');
 ?>
 
+<!-- Dishes Table -->
 <table class="table table-striped table-dark">
   <thead>
     <tr>
@@ -49,7 +58,7 @@ include('includes/navbar.php');
   </tbody>
 </table>
 
-
+<!-- Images Row -->
 <div class="container">
   <div class="row">
     <div class="col">
@@ -65,6 +74,10 @@ include('includes/navbar.php');
 </div>
 
 
+<!-- Link to Nav Button JS -->
+<script src="js/site.js"></script>
+
+<!-- Bootstrap -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
--- js/site.js
+++ js/site.js
@@ -1,19 +1,28 @@
-// Function to check time and show discount message
-function checkTimeForDiscount() {
-    const currentTime = new Date();
-    const currentMinute = currentTime.getMinutes();
-
-    if (currentMinute % 2 !== 0) {
-        // Minute is odd, display discount message
-        alert("Congratulations! You have a discount available.");
+// Function to check if the current time minute is odd
+function isOddMinute() {
+    const now = new Date();
+    const minute = now.getMinutes();
+    return minute % 2 !== 0;
+  }
+  
+  // Function to show a discount pop-up message
+  function showDiscountPopup() {
+    if (isOddMinute()) {
+      alert("Way to go! You have an available discount.");
     } else {
-        // Minute is even, no discount
-        alert("Sorry, no discount available at this time.");
+      alert("Sorry, no discount available right now.");
     }
-}
-
-// Add click event listener to the button
-const discountButton = document.getElementById("checkDiscountButton");
-if (discountButton) {
-    discountButton.addEventListener("click", checkTimeForDiscount);
-}
+  }
+  
+  // Add an event listener to the button in the navbar
+  document.addEventListener("DOMContentLoaded", function () {
+    const discountButton = document.getElementById("myButton");
+    if (discountButton) {
+      discountButton.addEventListener("click", showDiscountPopup);
+    }
+  });
+  
+  
+  
+  
+  
