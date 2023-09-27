21bb0f5c93fb3d85d0183a10bab337d0bae84f53
klhribal <klhribal@iu.edu>
2023-09-27 17:15:05 -0400

    Troubleshoot

--- public.php
+++ public.php
@@ -11,9 +11,70 @@
 <?php
 include('includes/navbar.php');
 ?>
-</body>
 
+
+
+<!-- Dishes Table -->
+<div class="dish-table>
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+        <th scope="col">Dish Name</th>
+        <th scope="col">Dish Price</th>
+        <th scope="col">Dish Category</th>
+        <th scope="col">Dish Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Strawberry Shortcake</th>
+      <td>$10.99</td>
+      <td>Cakes</td>
+      <td>This classic treat offers a harmonious blend of textures and flavors, with the sweet and tart strawberries complementing the light and airy cake or biscuits, all crowned by the richness of the whipped cream.</td>
+    </tr>
+    <tr>
+      <th scope="row">Peach Cobbler</th>
+      <td>$12.99</td>
+      <td>Baked Good</td>
+      <td>Indulge in the warmth of summer with our Peach Cobbler, a comforting dessert that captures the essence of ripe, juicy peaches. Served piping hot, our cobbler features succulent peach slices baked to perfection beneath a sweet, golden biscuit topping.</td>
+    </tr>
+    <tr>
+      <th scope="row">Creme Brulee</th>
+      <td>$7.99</td>
+      <td>Custard</td>
+      <td>Experience the epitome of elegance with our Crème Brûlée, a timeless French dessert. Delight in the silky smoothness of vanilla-infused custard, lovingly prepared to a velvety perfection. The pièce de résistance is the tantalizingly crisp, caramelized sugar crust that shatters with your first indulgent spoonful.</td>
+    </tr>
+    <tr>
+      <th scope="row">Cheesecake</th>
+      <td>$13.99</td>
+      <td>Cakes</td>
+      <td>Indulge in pure decadence with our Cheesecake, a luxurious dessert that defines creamy perfection. Our signature cheesecake is a masterful blend of rich cream cheese, sugar, and a hint of vanilla, baked to a velvety smoothness on a buttery graham cracker crust. </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<!-- Images Row -->
+<div class="container">
+  <div class="row">
+    <div class="col">
+    <img src="img/strawberry.jpg" alt="strawberry-shortcake">
+    </div>
+    <div class="col">
+    <img src="img/peach.jpg" alt="peach-cobbler">
+    </div>
+    <div class="col">
+    <img src="img/creme.jpg" alt="creme-brulee">
+    </div>
+  </div>
+</div>
+
+
+<!-- Link to Nav Button JS -->
+<script src="js/site.js"></script>
+
+<!-- Bootstrap -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </html>
